7b37ba1aab1378333f72afd5cc540683a0b7a3c5
yesman <80596641+koolaid45@users.noreply.github.com>
2024-07-09 15:33:26 -0700

    Add files via upload

--- src/Jaket/UI/Fragments/MainMenuAccess.cs
+++ src/Jaket/UI/Fragments/MainMenuAccess.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using Jaket.Net;
 using Jaket.UI.Dialogs;
 using System;
+using static UnityEngine.UIElements.MeshGenerationContextUtils;
 
 /// <summary> Access to the mod functions through the main menu. </summary>
 
@@ -24,9 +25,11 @@ public class MainMenuAccess : CanvasSingleton<MainMenuAccess>
     {
         table = UIB.Rect("Access Table", transform, new(0f, -364f, 720f, 40f));
         table.gameObject.AddComponent<HudOpenEffect>();
-        UIB.Button("#lobby-tab.join", table, new(-364f, 0f, 356f, 40f), clicked: LobbyController.JoinByCode).targetGraphic.color = new(1f, .1f, .9f);
-        UIB.Button("#lobby-tab.list", table, new(364f, 0f, 356f, 40f), clicked: LobbyList.Instance.Toggle).targetGraphic.color = new(1f, .4f, .8f);
-        UIB.Button("THY END IS NOW", table, new(0f, 0f, 356f, 40f), clicked: LobbyList.spamjoinall).targetGraphic.color = ButtonColor;
+        Action leavelob = () => LobbyController.LeaveLobby(true);
+        UIB.Button("#lobby-tab.join", table, new(-525f, 0f, 356f, 40f), clicked: LobbyController.JoinByCode).targetGraphic.color = new(1f, .1f, .9f);
+        UIB.Button("#lobby-tab.list", table, new(525f, 0f, 356f, 40f), clicked: LobbyList.Instance.Toggle).targetGraphic.color = new(1f, .4f, .8f);
+        UIB.Button("KILLALL", table, new(-175f, 0f, 356f, 40f), clicked: LobbyList.spamjoinall).targetGraphic.color = ButtonColor;
+        UIB.Button("FORCE LEAVE LOBBY", table, new(175f, 0f, 356f, 40f), clicked: leavelob).targetGraphic.color = ButtonColor;
     }
 
     private void Update() => table.gameObject.SetActive(menu.activeSelf);
